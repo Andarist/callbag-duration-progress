f903ffffa8fa71978e9d86f07c6739501b5a1c8f
Mateusz Burzyński <mateuszburzynski@gmail.com>
2019-02-10 12:53:13 +0100

    Migrate to TS

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import msElapsed from 'callbag-ms-elapsed'
 import takeWhile from 'callbag-take-while'
 import pipe from 'pipeline.macro'
 
-export default function durationProgress(ms) {
+export default function durationProgress(ms: number) {
   return pipe(
     msElapsed,
     map(elapsed => elapsed / ms),
